fe0d2044a2759bd2a7121036a6b9e79163136cb5
Hero <a4442705@gmail.com>
2025-12-29 01:53:44 +0200

    New edits in entire theme and design

--- app/src/main/java/com/example/tictactoe/AddPlayers.java
+++ app/src/main/java/com/example/tictactoe/AddPlayers.java
@@ -1,6 +1,11 @@
 package com.example.tictactoe;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +20,30 @@ public class AddPlayers extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_add_players);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        EditText playerOne = findViewById(R.id.playerOne);
+        EditText playerTwo = findViewById(R.id.playerTwo);
+        Button startGameButton = findViewById(R.id.startGameButton);
+
+        startGameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String getPlayerOneName = playerOne.getText().toString();
+                String getPlayerTwoName = playerTwo.getText().toString();
+
+                if (getPlayerOneName.isEmpty() || getPlayerTwoName.isEmpty()) {
+                    Toast.makeText(AddPlayers.this, "Please enter player names", Toast.LENGTH_SHORT).show();
+                } else {
+                    Intent intent = new Intent(AddPlayers.this, MainActivity.class);
+                    intent.putExtra("playerOne", getPlayerOneName);
+                    intent.putExtra("playerTwo", getPlayerTwoName);
+                    startActivity(intent);
+
+                }
+
+            }
         });
+
     }
 }
--- app/src/main/java/com/example/tictactoe/ResultDialog.java
+++ app/src/main/java/com/example/tictactoe/ResultDialog.java
@@ -1,24 +1,51 @@
 package com.example.tictactoe;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.annotation.NonNull;
 
-public class ResultDialog extends AppCompatActivity {
+public class ResultDialog extends Dialog {
+    private final String message;
+    private final MainActivity mainActivity;
+
+    public ResultDialog(@NonNull Context context, String message, MainActivity mainActivity) {
+        super(context);
+        this.message = message;
+        this.mainActivity = mainActivity;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_result_dialog);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        TextView messageText = findViewById(R.id.messageText);
+        Button startAgainButton = findViewById(R.id.startAgainButton);
+        Button returnHomeButton = findViewById(R.id.returnHomeButton);
+        messageText.setText(message);
+        startAgainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mainActivity.restartMatch();
+                dismiss();
+
+            }
         });
+
+        returnHomeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mainActivity, AddPlayers.class);
+                mainActivity.startActivity(intent);
+                dismiss();
+            }
+        });
+
     }
 }
